cbf9ff0908c45c7c26cd75182d17c11ff046a1de
Michael Goff <michaelgoff@Michaels-MacBook-Pro.local>
2021-08-29 17:19:22 -0700

    Documentation and other changes

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Edge Detection
+
+This is a simple demo that illustrates four edge detection algorithms in OpenCV.
+
+### Usage
+
+This program was run under the following versions.
+
+- `Python 3.9.5`
+- `OpenCV 4.5.2`
+- `Numpy 1.19.5`
+
+The main program folder should contain `main.py` and `kitty.jpg`. Navigate to the folder in OS X from the command line and run `python main.py`. This should work in other operating systems.
+
+The image in the upper left is the original image. In the upper right is the image with the selected edge detection algorithm applied. The algorithm can be chosen by clicking on the desired algorithm in the lower left. To adjust parameters (applicable only to the Canny and Sobel algorithms), click on the slider bar in the lower left.
+
+### Canny Edge Detection
+
+Canny edge detection is the most popular edge detection algorithm, and in this case it gives the crispest results. See the [OpenCV documentation](https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html) for details on how it works.
+
+A 5x5 Gaussian filter is applied first.
+
+The slider can be used to vary the minimum and maximum thresholds for edge detection. These values indicate the sensitivity with which edges are detected, with more edges detected for smaller values.
+
+### Sobel
+
+See [this presentation](https://www.cs.auckland.ac.nz/compsci373s1c/PatricesLectures/Edge%20detection-Sobel_2up.pdf) for details on the Sobel filter for edge detection. We apply a 5x5 Gaussian filter first.
+
+The upper slider indicates the Sobel kernel size, with possible values being 1, 3, 5, and 7.
+
+### Prewitt
+
+See this [code snippet](https://gist.github.com/rahit/c078cabc0a48f2570028bff397a9e154) for applying a Prewitt filter.
+
+### Laplacian
+
+See the [OpenCV documentation](https://docs.opencv.org/3.4/d5/db5/tutorial_laplace_operator.html) for information about the Laplacian filter.
+
+### About the Image
+
+The photograph is of Natsu, taken on April 24, 2021 when he was about eight weeks old. You can try this with other images as well.
--- main.py
+++ main.py
@@ -20,10 +20,10 @@ kitty = cv2.resize(kitty, (450,600), interpolation = cv2.INTER_AREA)
 # See the following for some code snippets on the Sobel filter.
 # https://gist.github.com/rahit/c078cabc0a48f2570028bff397a9e154
 gray_kitty = cv2.cvtColor(kitty, cv2.COLOR_BGR2GRAY)
-gaussian_kitty = cv2.GaussianBlur(gray_kitty,(3,3),0)
+gaussian_kitty = cv2.GaussianBlur(gray_kitty,(5,5),0)
 
 def dashboard():
-    full_img = 255*np.ones([100,2*kitty.shape[1],3 ], dtype=np.uint8)
+    full_img = 255*np.ones([150,2*kitty.shape[1],3 ], dtype=np.uint8)
     
     cv2.rectangle(
         full_img,
@@ -42,30 +42,56 @@ def dashboard():
     
     if display_params["algo"]=="canny":
         cv2.putText(full_img, "Canny", (710, 30), cv2.FONT_HERSHEY_PLAIN, 1.3, (255,0,0), 1, cv2.LINE_AA)
+        cv2.putText(full_img, "minVal is "+str(display_params["min"])+" and maxVal is "+str(display_params["max"])+ " for hysteresis thresholding.", (10, 130), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
     else:
         cv2.putText(full_img, "Canny", (710, 30), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
     if display_params["algo"]=="sobel":
         cv2.putText(full_img, "Sobel", (800, 30), cv2.FONT_HERSHEY_PLAIN, 1.3, (255,0,0), 1, cv2.LINE_AA)
+        cv2.putText(full_img, "Using a kernel size of "+str(sobel_kernel_size())+".", (10, 130), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
     else:
         cv2.putText(full_img, "Sobel", (800, 30), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
+    if display_params["algo"]=="prewitt":
+        cv2.putText(full_img, "Prewitt", (710, 70), cv2.FONT_HERSHEY_PLAIN, 1.3, (255,0,0), 1, cv2.LINE_AA)
+        cv2.putText(full_img, "No parameters to set.", (10, 130), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
+    else:
+        cv2.putText(full_img, "Prewitt", (710, 70), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
+    if display_params["algo"]=="laplacian":
+        cv2.putText(full_img, "Laplacian", (800, 70), cv2.FONT_HERSHEY_PLAIN, 1.3, (255,0,0), 1, cv2.LINE_AA)
+        cv2.putText(full_img, "No parameters to set.", (10, 130), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
+    else:
+        cv2.putText(full_img, "Laplacian", (800, 70), cv2.FONT_HERSHEY_PLAIN, 1.3, (0,0,0), 1, cv2.LINE_AA)
+    
     return full_img
 
 # Canvas
 canvas_height, canvas_width, no_channels = kitty.shape
 w = Canvas(master, 
            width=2*kitty.shape[1],
-           height=100+kitty.shape[0])
+           height=150+kitty.shape[0])
 w.pack()
+
+def sobel_kernel_size():
+    base_val = math.floor(display_params["max"]*4/650)
+    return 1+2*base_val
 
 def edge_detection():
     if display_params["algo"] == "canny":
-        return cv2.Canny(kitty,display_params["min"],display_params["max"])
-    else:
+        return cv2.Canny(gaussian_kitty,display_params["min"],display_params["max"])
+    if display_params["algo"] == "sobel":
         # A decent explanation of Sobel filters, including the kernel. https://www.cs.auckland.ac.nz/compsci373s1c/PatricesLectures/Edge%20detection-Sobel_2up.pdf
-        base_val = math.floor(display_params["max"]*4/650)
-        img_sobelx = cv2.Sobel(gaussian_kitty,cv2.CV_8U,1,0,ksize=1+2*base_val)
-        img_sobely = cv2.Sobel(gaussian_kitty,cv2.CV_8U,0,1,ksize=1+2*base_val)
+        kernel_size = sobel_kernel_size()
+        img_sobelx = cv2.Sobel(gaussian_kitty,cv2.CV_8U,1,0,ksize=kernel_size)
+        img_sobely = cv2.Sobel(gaussian_kitty,cv2.CV_8U,0,1,ksize=kernel_size)
         return (img_sobelx + img_sobely)
+    if display_params["algo"] == "prewitt":
+        kernelx = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+        kernely = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+        # Unclear whether the Gaussian blur should be applied.
+        img_prewittx = cv2.filter2D(gaussian_kitty, -1, kernelx)
+        img_prewitty = cv2.filter2D(gaussian_kitty, -1, kernely)
+        return img_prewittx+img_prewitty
+    if display_params["algo"] == "laplacian":
+        return cv2.Laplacian(gaussian_kitty,cv2.CV_64F)
 
 def make_image():
     # Exposition of canny edge detection here: https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html
@@ -109,8 +135,12 @@ def mouse_click(event):
     if (x >= 700):
         if x<780 and y < 50+kitty.shape[0]:
             display_params["algo"] = "canny"
-        if x>=780 and y < 50+kitty.shape[0]:
+        elif x>=780 and y < 50+kitty.shape[0]:
             display_params["algo"] = "sobel"
+        elif x<780 and y < 90+kitty.shape[0]:
+            display_params["algo"] = "prewitt"
+        elif x>=780 and y < 90+kitty.shape[0]:
+            display_params["algo"] = "laplacian"
     make_image()
     
 master.bind("<Button-1>", mouse_click)
